f3074de97d4a32a22e847ad5ae215353f6f27e4f
Wanda Schneid <schnewan@students.zhaw.zh>
2025-04-27 22:53:17 +0200

    Button

--- Start.py
+++ Start.py
@@ -1,23 +1,16 @@
 import streamlit as st
-from utils.data_manager import DataManager  # Lokaler Manager für Login
-from utils.dual_data_manager import DualDataManager  # Dualer Manager für Userdaten
+from utils.data_manager import DataManager
+from utils.dual_data_manager import DualDataManager
 from utils.login_manager import LoginManager
-from streamlit_extras.switch_page_button import switch_page  # für echte Navigation
+from streamlit_extras.switch_page_button import switch_page
 
 # --- Seitenkonfiguration ---
 st.set_page_config(page_title="Gesundheits-Tracker", page_icon="💪", layout="centered")
 
 # --- Initialisierung ---
-# Lokaler Manager NUR für Login
 local_data_manager = DataManager(fs_protocol='file', fs_root_folder="app_data")
-
-# DualManager für User-Daten
 data_manager = DualDataManager()
-
-# LoginManager bekommt den lokalen DataManager
 login_manager = LoginManager(data_manager=local_data_manager)
-
-# Login/Register
 login_manager.login_register()
 
 # --- Hauptbereich ---
@@ -40,11 +33,11 @@ st.markdown("""
     </style>
 """, unsafe_allow_html=True)
 
-# Navigation
+# --- Buttons für Navigation ---
 col1, col2, col3 = st.columns([1, 2, 1])
 with col2:
     if st.button("🍎 :green[Ernährung]"):
-        switch_page("ernaehrung")   # Kein "1_" mehr!
+        switch_page("ernaehrung")  # <- exakt wie der Dateiname ohne ".py"
 
     if st.button("🏃 :orange[Bewegung]"):
         switch_page("bewegung")
