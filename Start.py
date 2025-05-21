30ebe6a7015f9a977a3a6e74e374084ea243a5da
Wanda Schneid <schnewan@students.zhaw.zh>
2025-05-21 08:24:42 +0200

    rückgängig

--- Start.py
+++ Start.py
@@ -28,7 +28,7 @@ if 'username' not in st.session_state:
     login_manager.login_register()
     st.stop()  # ⛔️ Stoppt den Seitenaufbau, bis Login erfolgt ist
 
-# --- Daten laden und Speicherung auf Switch Drive ---
+# --- Daten laden nach Login---
 data_manager.load_user_data(
     session_state_key='data_df', 
     file_name='data.csv', 
